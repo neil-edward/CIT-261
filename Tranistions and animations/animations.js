25aaa5f35c0adb4fc59cf851b6f15b7755e7ba9f
neil-edward <eddie.neil@gmail.com>
2019-02-28 15:03:06 -0500

    c

--- Tranistions and animations/animations.js
+++ Tranistions and animations/animations.js
@@ -1,18 +1,18 @@
 
-function startRace() {
+function peek() {
 
-    var a = document.getElementById("div1");
-    var b = document.getElementById("div2");
-    var c = document.getElementById("div3");
-    var d = document.getElementById("div4");
-    var player = document.getElementById("div5");
-      
-
-
-  a.style.animation = "mymove 10s 1";
-  b.style.animation = "mymove 7s 1";
-  c.style.animation = "mymove 8s 1";
-  d.style.animation = "mymove 12s 1";
-  player.style.animation = "mymove 15s 1";
+  var a = document.getElementById("div1");
+  a.style.animation = "peek 2s";
   
+  
+}
+
+function shy() {
+
+  var b = document.getElementById("div2");
+  b.style.animation = "move 3s 1";
+
+  alert("Oh no! You Scared it!");
+
+  document.getElementById("shyGuy").innerHTML = "It's feeling better now";
 }
