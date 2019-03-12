72eae9d2036dcf529bf3ea79d9c0d22084756ea7
neil-edward <eddie.neil@gmail.com>
2019-03-12 13:33:07 -0400

    Try again

--- LocalStorage/storage.js
+++ LocalStorage/storage.js
@@ -1,27 +1,40 @@
+function Settings(color,time, birth) {
+    this.color = color;
+    this.time = time;
+    this.birth = birth;
 
+  }
 
 function getSettings(){
-document.getElementById("settings").innerHTML = localStorage.color + "<br>" + localStorage.time + "<br>" + localStorage.birth;
+    var storedObject = JSON.parse(localStorage.getItem('object'));
+document.getElementById("settings").innerHTML = localStorage.color + "<br>" + localStorage.time + "<br>" + localStorage.birth + "<br>" + storedObject.one;
 }
 
 function submit() {
-
-
-
     if (typeof(Storage) !== "undefined") {
 
-        var color = document.forms['color']['input'].value;
-        var time = document.forms['time']['input'].value;
-        var birth = document.forms['birth']['input'].value;
+        var user1 = new Settings();
+        user1.color = document.forms['color']['input'].value;
+        user1.time = document.forms['time']['input'].value;
+        user1.birth = document.forms['birth']['input'].value;
+
+
+
         
     
 
         
-        localStorage.setItem("color", color);
-        localStorage.setItem("time", time);
-        localStorage.birth = birth
+        localStorage.setItem("color", user1.color);
+        localStorage.setItem("time", user1.time);
+        localStorage.birth = user1.birth
 
-        document.getElementById("settings").innerHTML = localStorage.getItem("color") + "<br>" + localStorage.time + "<br>" + localStorage.birth;
+
+        //This is the object
+        var object = { 'one': 1, 'two': 2, 'three': 3 };
+        localStorage.setItem('object', JSON.stringify(object));
+        var storedObject = JSON.parse(localStorage.getItem('object'));
+
+        document.getElementById("settings").innerHTML = localStorage.getItem("color") + "<br>" + localStorage.time + "<br>" + localStorage.birth + "<br>" + storedObject.one;
     }
     else {
         document.getElementById("settings").innerHTML = "Could not getting settings data";
