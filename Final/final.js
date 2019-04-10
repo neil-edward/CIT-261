a98dde677078bc70b53f57fe26cc6322d0ed355e
neil-edward <eddie.neil@gmail.com>
2019-04-10 18:47:40 -0400

    Final

---
+++ Final/final.js
@@ -0,0 +1,31 @@
+function fillContentAction()
+{
+    document.getElementById("info").innerHTML = "HE"
+}
+
+function fillContentComedy()
+{
+
+}
+
+function fillContentDrama()
+{
+}
+
+function fillContentChildren()
+{
+
+
+}
+
+function fillContentHorror()
+{
+
+}
+
+
+function fillInfo(num)
+{
+    var id = num.id;
+    document.getElementById("info").innerHTML = id;
+}
